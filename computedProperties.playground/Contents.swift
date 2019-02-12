e1fea595f53f33bd828cc95e4e9fd407dcbff853
Bryce Hull <bahull@live.com>
2019-02-11 18:37:27 -0600

    Get/Set methods

--- computedProperties.playground/Contents.swift
+++ computedProperties.playground/Contents.swift
@@ -1,3 +1,22 @@
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+let pizzaInInches : Int = 10
+
+var numberOfSlices : Int {
+    get {
+        return pizzaInInches - 4
+    }
+    set {
+        print("Number of slice now has a new value which is\(newValue)")
+    }
+}
+
+numberOfSlices = 12
+
+
+
+print(pizzaInInches)
+
+let a = numberOfSlices * 2
+
+
