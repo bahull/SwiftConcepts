ed74ad1cc7aef0a02da53a51ade2a1e4ea3860cc
Bryce Hull <bahull@live.com>
2019-02-11 18:47:09 -0600

    Observe property problem

--- computedProperties.playground/Contents.swift
+++ computedProperties.playground/Contents.swift
@@ -1,22 +1,68 @@
 import Foundation
 
-let pizzaInInches : Int = 10
-
+//MARK: - Getter/Setter properties
+let pizzaInInches : Int = 12
+var numberOfPeople : Int = 6
+let slicesPerPerson : Int = 5
 var numberOfSlices : Int {
     get {
         return pizzaInInches - 4
     }
+}
+
+var numberOfPizza : Int {
+    get {
+        let numberOfPeopleFedPerPizza = numberOfSlices / slicesPerPerson
+        return numberOfPeople / numberOfPeopleFedPerPizza
+    }
     set {
-        print("Number of slice now has a new value which is\(newValue)")
+        let totalSlices = numberOfSlices * newValue
+        numberOfPeople = totalSlices / slicesPerPerson
     }
 }
 
-numberOfSlices = 12
+numberOfPizza = 4
+
+print(numberOfPizza)
 
 
+//MARK: - Oberved properties
+var pizzaInInches : Int = 10 {
+    willSet {
+        //Fires right before value changes
+        print(pizzaInInches)
+        print(newValue)
+        
+    }
+    didSet {
+        //Changes right after value is set
+        print(pizzaInInches)
+        print(oldValue)
+        if pizzaInInches >= 18 {
+            print("Invalid size specified, pizzaInInches set to 18.")
+            pizzaInInches = 18
+        }
+    }
+}
+var numberOfPeople : Int = 6
+let slicesPerPerson : Int = 5
+var numberOfSlices : Int {
+    get {
+        return pizzaInInches - 4
+    }
+}
 
-print(pizzaInInches)
+var numberOfPizza : Int {
+    get {
+        let numberOfPeopleFedPerPizza = numberOfSlices / slicesPerPerson
+        return numberOfPeople / numberOfPeopleFedPerPizza
+    }
+    set {
+        let totalSlices = numberOfSlices * newValue
+        numberOfPeople = totalSlices / slicesPerPerson
+    }
+}
 
-let a = numberOfSlices * 2
+numberOfPizza = 4
 
-
+print(numberOfPizza)
